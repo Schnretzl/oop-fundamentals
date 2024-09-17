ac1b6635216d1c4e6e5b2e21a9db0ec597795302
Schnretzl <wrhendley@gmail.com>
2024-09-17 16:16:31 -0500

    Update with task 2

--- problem1.py
+++ problem1.py
@@ -10,3 +10,12 @@ my_new_car.update_owner("Wesley")
 
 print(f"I sold my car to {my_old_car.owner}.")
 print(f"I purchased car {my_new_car.registration_number}, so it now belongs to {my_new_car.owner}!")
+
+
+# Task 2: Event Management System Enhancement
+from event import Event
+
+my_event = Event("Birthday party", "02/03/2024", 10)
+my_event.add_participant()
+my_event.add_participant()
+print(f"I had my {my_event.name} on {my_event.date}, and {my_event.get_participant_count()} people attended!")
